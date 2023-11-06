7fe0aadae65947ac55de5711802b51e2c83bb5a1
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-06 09:22:46 +0530

    opacity

--- components/Fragments/HeroSection.js
+++ components/Fragments/HeroSection.js
@@ -1,19 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 function HeroSection() {
   return (
-    <section className="pt-[160px] lg:pt-[218px] px-[20px] lg:px-[100px] bg-[url('/mesh-1.png')] min-h-[800px]">
-      <h1 className="headingGradient text-[40px] lg:text-[72px] text-center font-bold leading-[50px] lg:leading-[90.36px] relative">
-        Every Design Woven <br /> Into A Story
-      </h1>
-      <p className="text-white text-center mt-7 leading-[24px]">
-        We play with colors and elements to create <br /> visually amazing shit
-        for your brand.
-      </p>
-      <div className="flex items-center justify-center mt-12">
-        <button className="radialG h-[48px] px-[75px] flex items-center text-white rounded-lg border-[1px] border-[#BAB4FF]">
-          Contact Us
-        </button>
+    <section className="min-h-[800px] relative">
+      <img
+        src="/mesh-1.png"
+        alt=""
+        className="absolute inset-0 h-full w-full z-0 opacity-50"
+      />
+      <div className="absolute inset-0 h-full w-full z-20 pt-[160px] lg:pt-[218px] px-[20px] lg:px-[100px]  ">
+        <h1 className="headingGradient text-[40px] lg:text-[72px] text-center font-bold leading-[50px] lg:leading-[90.36px] relative">
+          Every Design Woven <br /> Into A Story
+        </h1>
+        <p className="text-white text-center mt-7 leading-[24px] z-30">
+          We play with colors and elements to create <br /> visually amazing
+          shit for your brand.
+        </p>
+        <div className="flex items-center justify-center mt-12 z-20">
+          <button className="radialG h-[48px] px-[75px] flex items-center text-white rounded-lg border-[1px] border-[#BAB4FF]">
+            Contact Us
+          </button>
+        </div>
       </div>
     </section>
   );
