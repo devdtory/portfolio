60b4b05807d18b2a00bc09752d79c6e0266c8aa0
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-23 20:54:44 +0530

    pricing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import FAQ from "@/components/Fragments/FAQ";
 import HeroSection from "@/components/Fragments/HeroSection";
 import PeopleSay from "@/components/Fragments/PeopleSay";
+import Pricing from "@/components/Fragments/Pricing";
 import Process from "@/components/Fragments/Process";
 import Services from "@/components/Fragments/Services";
 import TrustedBy from "@/components/Fragments/TrustedBy";
@@ -17,6 +18,8 @@ export default function Home() {
       <Process />
       <div className="h-[180px] lg:h-[250px]"></div>
       <Services />
+      <div className="h-[185px]"></div>
+      <Pricing />
       <div className="h-[185px]"></div>
       <Works />
       <div className="h-[150px]"></div>
