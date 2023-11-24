b8a074b013ec32ffa43601970610d069a3dcec12
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-24 14:15:32 +0530

    faq

--- components/Fragments/FAQ.js
+++ components/Fragments/FAQ.js
@@ -1,22 +1,77 @@
+import { Accordion, AccordionItem } from "@nextui-org/react";
 import React from "react";
 
 function FAQ() {
   return (
     <div className="font-mona-sans">
       <h1 className="text-white font-bold text-center text-[40px]">FAQs</h1>
-      <div className="px-[30px] lg:px-[250px] mt-7">
-        <div className="flex items-center justify-between py-6 border-b">
-          <span className="text-white">
-            Why should I not hire an individual member instead ?
-          </span>
-          <button className="text-white">+</button>
-        </div>
-        <div className="flex items-center justify-between py-6 border-b">
-          <span className="text-white">
-            Why should I not hire an individual member instead ?
-          </span>
-          <button className="text-white">+</button>
-        </div>
+      <div className="px-[30px] lg:px-[250px] mt-7 font-mona-sans">
+        <Accordion isCompact={false} className="space-y-2">
+          <AccordionItem
+            className="border-b border-[#D6D6D6]"
+            key="1"
+            aria-label="01. UX / Wireframing"
+            title={
+              <div className="flex items-center text-xl text-white">
+                What is the refund policy ?
+              </div>
+            }
+          >
+            <p className="pb-4 text-sm text-left leading-7 text-neutral-300 -mt-2">
+              Owing to the superior quality of the work and time put in, we do
+              not entertain refund requests.
+            </p>
+          </AccordionItem>
+          <AccordionItem
+            className="border-b border-[#D6D6D6]"
+            key="2"
+            aria-label="01. UX / Wireframing"
+            title={
+              <div className="flex items-center text-xl text-white">
+                Do you offer e-commerce solutions for online businesses?
+              </div>
+            }
+          >
+            <p className="pb-4 text-sm text-left leading-7 text-neutral-300 -mt-2">
+              Absolutely! Our e-commerce solutions deliver seamless online
+              shopping experiences, integrating cutting-edge technology for
+              secure transactions and user-friendly interfaces to elevate your
+              business.
+            </p>
+          </AccordionItem>
+          <AccordionItem
+            className="border-b border-[#D6D6D6]"
+            key="3"
+            aria-label="01. UX / Wireframing"
+            title={
+              <div className="flex items-center text-xl text-white">
+                What if I don&apos;t like the design ?
+              </div>
+            }
+          >
+            <p className="pb-4 text-sm text-left leading-7 text-neutral-300 -mt-2">
+              No problem! We&apos;re committed to refining the design until it
+              meets your expectations completely. Your satisfaction is our
+              priority.
+            </p>
+          </AccordionItem>
+          <AccordionItem
+            className="border-b border-[#D6D6D6]"
+            key="4"
+            aria-label="01. UX / Wireframing"
+            title={
+              <div className="flex items-center text-xl text-white">
+                Do you offer ongoing support and maintenance after the project
+                is completed?
+              </div>
+            }
+          >
+            <p className="pb-4 text-sm text-left leading-7 text-neutral-300 -mt-2">
+              Yes we have an annual maintenance package which you can subscribe
+              for support and maintenance after completion of the project.
+            </p>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );
