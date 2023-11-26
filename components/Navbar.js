e385d1c2737c11aecd0f7a31e9942650e4b31728
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-11-26 19:13:42 +0530

    sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -55,21 +55,35 @@ function Navbar() {
                 onClick={() => setSidebarOpen(!sidebarOpen)}
                 className="lg:hidden"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 32 32"
-                  fill="none"
-                >
-                  <path
-                    d="M5.33203 10.6641H26.6654M5.33203 21.3307H26.6654"
-                    stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                {sidebarOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="white"
+                      d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                  >
+                    <path
+                      d="M5.33203 10.6641H26.6654M5.33203 21.3307H26.6654"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                )}
               </button>
               <button className="radialG h-[48px] px-6 hidden lg:flex items-center text-white rounded-lg">
                 Contact us
