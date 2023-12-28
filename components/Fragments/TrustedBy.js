a6c1532001d2c31bc31fa586df3e371000964c84
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-12-28 20:16:37 +0530

    trusted by

--- components/Fragments/TrustedBy.js
+++ components/Fragments/TrustedBy.js
@@ -104,28 +104,28 @@ function TrustedBy() {
 
       <div className="grid gap-12 lg:hidden grid-cols-2 mt-16 place-content-center place-items-center">
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699211401/portfolio/hoot_logo_yo2gvq.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703770060/rhfdpxlukk6vlh5ywabo.webp"
           alt=""
           className="h-16 grayscale object-contain col-span-2"
         />
 
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699211401/portfolio/Clients_devpriyangsu_Image_ryuohj.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703771914/ufgtbx9xwehkghjro1be.webp"
           alt=""
           className="h-10 grayscale object-contain"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699211400/portfolio/Level_Box_Games_Logo_w2w23e.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703770164/q7fpqwcoebnbhrjyznvp.webp"
           alt=""
           className="h-9 grayscale object-contain"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699211400/portfolio/richribbit_t8srjf.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703770164/wlw4hoes7us2gbyb1qnw.webp"
           alt=""
           className="h-20 grayscale object-contain"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699211400/portfolio/Rocketbird_1_._fuoyin.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703770165/vf9uj8scibe981ujdxoi.webp"
           alt=""
           className="h-14 grayscale object-contain"
         />
@@ -135,17 +135,17 @@ function TrustedBy() {
           className="h-10 grayscale object-contain"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699555776/portfolio/Hello_Legal_Logo_afxhpb.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703771766/zrcw0m6d2botlkkqlwed.webp"
           alt=""
           className="h-10 grayscale object-contain"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1699555775/portfolio/elmo_logo_wogfua.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703770072/hahcxlxubhievqd6nb3x.webp"
           alt=""
           className="h-10 grayscale object-contain col-span-2"
         />
         <img
-          src="https://res.cloudinary.com/dxfa5a3ya/image/upload/v1701083782/portfolio/Meme_Alliance_Logo_k1apae.webp"
+          src="https://res.cloudinary.com/davpsqfkq/image/upload/v1703773078/ohbvagn2be7vybscljfr.webp"
           className="h-10 grayscale object-contain col-span-2"
           alt=""
         />
